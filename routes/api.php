ca1a4cb133bc7215666b962d40d99b60d4c4bede
Abel <abel291m@gmail.com>
2021-11-01 02:28:05 -0500

    cookie to token authentication

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\CardProductController;
 use App\Http\Controllers\Api\PageController;
 use App\Http\Controllers\Api\ProductCheckoutController;
 use App\Http\Controllers\Api\SearchController;
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,19 +19,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    
-    Route::get('/user', function (Request $request) {
-        return auth()->user();
+
+Route::prefix('auth')->group(function () {
+    //register new user
+    Route::post('/register', [AuthenticationController::class, 'register']);
+    //login user
+    Route::post('/login', [AuthenticationController::class, 'login']);
+    //using middleware
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+
+        Route::get('/profile', function (Request $request) {
+            return auth()->user();
+        });
+        Route::post('/logout', [AuthenticationController::class, 'logout']);
+
+        Route::put('/user/profile-information', [AuthenticationController::class, 'profile_information']);
+
+        Route::put('/user/password', [AuthenticationController::class, 'update_user_password']);
     });
+});
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
     Route::post('/product-checkout', [ProductCheckoutController::class, 'product_checkout']);
     Route::get('/product-checkout-cart', [ProductCheckoutController::class, 'product_checkout_cart']);
     Route::resource('card-products', CardProductController::class)->only([
         'index', 'create', 'store', 'destroy'
     ]);
-    
 });
-
 
 Route::get('/search', [SearchController::class, 'search']);
 Route::get('/init', [PageController::class, 'init']);
@@ -40,3 +57,10 @@ Route::get('/page/assemblies', [PageController::class, 'assemblies']);
 Route::get('/page/combos', [PageController::class, 'combos']);
 
 Route::get('/product/{id}/{slug}', [PageController::class, 'product']);
+
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
